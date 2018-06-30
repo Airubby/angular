14268b33850fae8df297c9050c70ceb7821d7a1e
Airubby <ai_rubby@qq.com>
2018-06-30 15:27:11 +0800

    商品跳转路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
 import { StarsComponent } from './stars/stars.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
-  declarations: [ //只能声明指令，组件和管道, NavbarComponent, FooterComponent, SearchComponent, CarouselComponent, ProductComponent, StarsComponent
+  //只能声明指令，组件和管道, 
+  declarations: [ 
     AppComponent,
     NavbarComponent,
     FooterComponent,
     SearchComponent,
     CarouselComponent,
     ProductComponent,
-    StarsComponent
+    ProductDetailComponent,
+    HomeComponent,
+    StarsComponent,
   ],
+  //浏览器模块  FormsModule,HttpModule
   imports: [
-    BrowserModule  //浏览器模块  FormsModule,HttpModule
+    BrowserModule,
+    AppRoutingModule  
   ],
-  providers: [], //声明服务
+  //声明服务
+  providers: [], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
